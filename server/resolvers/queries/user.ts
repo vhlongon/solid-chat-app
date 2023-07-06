a0b787508c17579c435821e973a30850950e869e
vhlongon <vhlongon@gmail.com>
2023-07-06 20:48:16 +0200

    fix linting issue and add tsc script

--- server/resolvers/queries/user.ts
+++ server/resolvers/queries/user.ts
@@ -1,5 +1,5 @@
 import { Resolvers } from '../../../generated/resolvers-types';
 import { usersData } from '../../data';
 
-export const user: Resolvers['Query']['user'] = (_, { id }, { pubSub }) =>
+export const user: Resolvers['Query']['user'] = (_, { id }) =>
   usersData.find((user) => user.id === id) || null;
