7c52dcd8d92488af7e284ae8edf924cf6bf9326d
vhlongon <vhlongon@gmail.com>
2023-07-09 21:58:22 +0200

    add ws support for gql client

--- client/gqlClient.ts
+++ client/gqlClient.ts
@@ -1,6 +1,34 @@
-import { createClient, cacheExchange, fetchExchange } from '@urql/core';
+import {
+  createClient,
+  cacheExchange,
+  fetchExchange,
+  subscriptionExchange,
+} from '@urql/core';
+import { createClient as createWSClient, SubscribePayload } from 'graphql-ws';
+
+const wsClient = createWSClient({
+  url: import.meta.env.VITE_GRAPHQL_ENDPOINT_WS,
+});
 
 export const client = createClient({
-  url: 'http://localhost:4000/graphql',
-  exchanges: [cacheExchange, fetchExchange],
+  url: import.meta.env.VITE_GRAPHQL_ENDPOINT,
+  exchanges: [
+    cacheExchange,
+    fetchExchange,
+    subscriptionExchange({
+      forwardSubscription(operation) {
+        return {
+          subscribe: (sink) => {
+            const dispose = wsClient.subscribe(
+              operation as SubscribePayload,
+              sink
+            );
+            return {
+              unsubscribe: dispose,
+            };
+          },
+        };
+      },
+    }),
+  ],
 });
