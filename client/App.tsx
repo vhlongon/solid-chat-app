df5f7700f45b7b5fad76ce1221c0bceec4ac4f4b
vhlongon <vhlongon@gmail.com>
2023-08-08 21:30:44 +0200

    catch all route to main component

--- client/App.tsx
+++ client/App.tsx
@@ -9,7 +9,7 @@ export const App = () => {
     <Routes>
       <Route path="/" component={Chat} />
       <Route path="/login" component={Login} />
-      <Route path="*" component={Login} />
+      <Route path="*" component={Chat} />
     </Routes>
   );
 };
