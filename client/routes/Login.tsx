d740a15d4c507f89ebf4a98b9c5d1a56d136a528
vhlongon <vhlongon@gmail.com>
2023-08-08 20:50:31 +0200

    add loading state and start script

--- client/routes/Login.tsx
+++ client/routes/Login.tsx
@@ -1,27 +1,35 @@
 import { useNavigate, useSearchParams } from '@solidjs/router';
 import { CombinedError } from '@urql/core';
-import { createEffect, createSignal } from 'solid-js';
+import { Show, createEffect, createSignal } from 'solid-js';
 import { ErrrorBox } from '../components/ErrrorBox';
 import { authenticateUser, getAuthUrl } from '../data';
 import { setAuthToken } from '../storage';
+import { Spinner } from '../components/Spinner';
 
 export const Login = () => {
   const [errors, setErrors] = createSignal<Error | CombinedError | null>(null);
+  const [loading, setLoading] = createSignal(false);
   const navigate = useNavigate();
 
   const [searchParams] = useSearchParams<{ code: string }>();
 
-  const handleAccessToken = (accessToken: string) => {
-    if (accessToken) {
-      authenticateUser(accessToken, {
-        onError: setErrors,
-      }).then((res) => {
+  const handleAccessToken = async (accessToken: string) => {
+    if (!accessToken) return;
+
+    setLoading(true);
+
+    authenticateUser(accessToken, {
+      onError: setErrors,
+    })
+      .then((res) => {
         if (res?.token) {
           setAuthToken(res.token);
           navigate('/');
         }
+      })
+      .finally(() => {
+        setLoading(false);
       });
-    }
   };
 
   createEffect(() => {
@@ -41,18 +49,20 @@ export const Login = () => {
   };
   return (
     <div class="h-screen w-screen flex justify-center items-center">
-      <div class="card">
-        <div class="card-body">
-          <h2 class="card-header">Login with github to chat</h2>
-          <p class="text-content2">It will only access your public information</p>
-          <div class="card-footer">
-            <button onClick={onLogin} class="btn btn-secondary bt-lg">
-              Login
-            </button>
+      <Show when={!loading()} fallback={<Spinner>Loading chat</Spinner>}>
+        <div class="card">
+          <div class="card-body">
+            <h2 class="card-header">Login with github to chat</h2>
+            <p class="text-content2">It will only access your public information</p>
+            <div class="card-footer">
+              <button onClick={onLogin} class="btn btn-secondary bt-lg">
+                Login
+              </button>
+            </div>
           </div>
+          <ErrrorBox message={errors()?.message} />
         </div>
-        <ErrrorBox message={errors()?.message} />
-      </div>
+      </Show>
     </div>
   );
 };
